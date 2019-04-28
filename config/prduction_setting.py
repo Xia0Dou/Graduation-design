25747d2bb3caac4dfa9ff8e2fa8b5224267759af
xiao豆 <42510967+Xia0Dou@users.noreply.github.com>
2019-04-28 16:14:07 +0800

    Update prduction_setting.py

---
+++ config/prduction_setting.py
@@ -0,0 +1,12 @@
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_ENCODING = "utf-8"
+SQLALCHEMY_DATABASE_URI = "mysql://root:123456@127.0.0.1/food_db?charset=utf8mb4"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
+
+APP = {
+    'domain': 'https://foodpy.xyz'
+}
+
+RELEASE_VERSION="20190427001"
